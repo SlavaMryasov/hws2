1e38c11f3504a0db4109684387da3b249089b530
SlavaMryasov <slavadevbox1@gmail.com>
2024-05-28 21:23:20 +0300

    HW9

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -8,6 +8,7 @@ import HW5 from '../s2-homeworks/hw05/HW5'
 import HW6 from '../s2-homeworks/hw06/HW6'
 import HW7 from '../s2-homeworks/hw07/HW7'
 import HW8 from '../s2-homeworks/hw08/HW8'
+import HW9 from '../s2-homeworks/hw09/HW9'
 
 function App() {
     return (
@@ -19,10 +20,11 @@ function App() {
             {/* <HW4 /> */}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            <HW5 />
+            {/* <HW5 />
             <HW6 />
             <HW7 />
-            <HW8 />
+            <HW8 /> */}
+            <HW9 />
         </div>
     )
 }
--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import {restoreState} from '../hw06/localStorage/localStorage'
+import { restoreState } from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
 
 function Clock() {
@@ -12,27 +12,46 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
+        const id = setInterval(() => setDate(new Date()), 1000)
+        setTimerId(id as unknown as number)
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
+        clearInterval(timerId)
+        setTimerId(undefined)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+        setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+        setShow(false)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+    const time = (value: number) => {
+        return value < 10 ? '0' + value : value
+    }
+
+    const stringTime = `${time(date.getHours())}:${time(date.getMinutes())}:${time(date.getSeconds())}` || <br /> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = `${time(date.getDay())}:${time(date.getMonth())}:${time(date.getFullYear())}` || <br />
+
+    // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const day = () => {
+        const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        return dayArray[date.getDay()]
+    }
+
+    const month = () => {
+        const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return monthsArray[date.getMonth()]
+    }
+
+    const stringDay = day() || <br /> // пишут студенты
+    const stringMonth = month() || <br /> // пишут студенты
 
     return (
         <div className={s.clock}>
@@ -43,7 +62,7 @@ function Clock() {
                 onMouseLeave={onMouseLeave}
             >
                 <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
+                <span onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
             </div>
@@ -57,7 +76,7 @@ function Clock() {
                         </>
                     ) : (
                         <>
-                            <br/>
+                            <br />
                         </>
                     )}
                 </div>
@@ -66,14 +85,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={!!timerId} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
