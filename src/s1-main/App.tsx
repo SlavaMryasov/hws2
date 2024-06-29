beeca4037563f5f2ee8707d755cd53812e3b369c
SlavaMryasov <slavadevbox1@gmail.com>
2024-06-29 20:38:39 +0300

    HW14

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -13,6 +13,7 @@ import HW10 from '../s2-homeworks/hw10/HW10'
 import HW11 from '../s2-homeworks/hw11/HW11'
 import HW12 from '../s2-homeworks/hw12/HW12'
 import HW13 from '../s2-homeworks/hw13/HW13'
+import HW14 from '../s2-homeworks/hw14/HW14'
 
 function App() {
     return (
@@ -25,7 +26,7 @@ function App() {
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5 />
-            <HW6 />
+            {/* <HW6 />
             <HW7 />
             <HW8 />
             <HW9 />
@@ -33,6 +34,7 @@ function App() {
             <HW11 />
             <HW12 />
             <HW13 />
+            <HW14 /> */}
         </div>
     )
 }
